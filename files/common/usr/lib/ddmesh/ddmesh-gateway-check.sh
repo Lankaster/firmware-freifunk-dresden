0277da6820fab6bc9ffad70161aab4f6283465ad
Yaroslav Petrov <info@lank.me>
2019-08-31 10:36:40 +0200

    GW Check

--- files/common/usr/lib/ddmesh/ddmesh-gateway-check.sh
+++ files/common/usr/lib/ddmesh/ddmesh-gateway-check.sh
@@ -71,7 +71,7 @@ done
 $DEBUG && echo "start"
 
 #dont use vpn server (or any openvpn server), it could interrupt connection
-ping_vpn_hosts="85.114.135.114 178.254.18.30 5.45.106.241 178.63.61.147 82.165.230.17 1.1.1.1 8.8.4.4 8.8.8.8 178.63.61.147"
+ping_vpn_hosts="85.114.135.114 178.254.18.30 85.14.253.99 46.38.243.230 5.45.106.241 84.200.50.17 46.105.31.203 84.200.85.38 109.73.51.35 178.63.61.147"
 #ping_vpn_hosts="85.14.253.99 46.38.243.230 5.45.106.241 84.200.50.17 46.105.31.203 84.200.85.38 109.73.51.35 178.63.61.147"
 ping_hosts="$ping_vpn_hosts 8.8.8.8"
 #process max 3 user ping
@@ -80,15 +80,6 @@ gw_ping="$(echo "$cfg_ping" | sed 's#[ ,;/	]\+# #g' | cut -d' ' -f1-3 ) $ping_ho
 $DEBUG && echo "hosts:[$gw_ping]"
 
 #determine all possible gateways
-network_is_up wwan  && {
-	#get network infos using /lib/functions/network.sh
-	network_get_device default_wwan_ifname wwan
-	default_wwan_gateway=$(ip route | sed -n "/default via [0-9.]\+ dev $default_wwan_ifname/{s#.*via \([0-9.]\+\).*#\1#p}")
-	if [ -n "$default_wwan_gateway" -a -n "$default_wwan_ifname" ]; then
-		wwan_default_route="$default_wwan_gateway:$default_wwan_ifname"
-	fi
-}
-echo "WWAN:$default_wwan_ifname via $default_wwan_gateway"
 
 network_is_up wan  && {
 	#get network infos using /lib/functions/network.sh
@@ -124,10 +115,9 @@ echo "VPN:$default_vpn_ifname via $default_vpn_gateway"
 #try each gateway
 ok=false
 IFS=' '
-# start with vpn, because this is prefered gateway, then WAN and lates LAN
-# (there is no forwarding to lan allowed by firewall)
-# wwan after wan: assume wan is faster than wwan
-for g in $vpn_default_route $wan_default_route $wwan_default_route $lan_default_route
+#start with vpn, because this is prefered gateway, then WAN and lates LAN
+#(there is no forwarding to lan allowed by firewall)
+for g in $vpn_default_route $wan_default_route $lan_default_route
 do
 	echo "==========="
 	echo "try: $g"
@@ -157,10 +147,11 @@ do
 	do
 		$DEBUG && echo "add ping route ip:$ip"
 		ip route add $ip via $via dev $dev table ping
-		$DEBUG && echo ip route add $ip via $via dev $dev table ping
+		$DEBUG && echo "route:$(ip route get $ip)"
+		$DEBUG && echo "route via:$(ip route get $via)"
 
 		# mark only tested ip addresses
-		iptables -w -t mangle -A output_gateway_check -p icmp -d $ip -j MARK --set-mark $ip_fwmark
+		iptables -t mangle -A output_gateway_check -p icmp -d $ip -j MARK --set-mark $ip_fwmark
 		numIPs=$((numIPs+1))
 	done
 	echo "number IPs: $numIPs"
@@ -175,9 +166,7 @@ do
 	#run check
 	ok=false
 	countSuccessful=0
-	minSuccessful=$(( (numIPs+1)/2 ))
-	if [ $minSuccessful -lt 4 ]; then minSuccessful=4; fi
-	echo "minSuccessful: $minSuccessful"
+	minSuccessful=1
 
 	IFS=' '
 	for ip in $gw_ping
@@ -193,11 +182,10 @@ do
 		fi
 	done
 	if $ok; then
-		$DEBUG && echo "gateway found: via [$via] dev [$dev]"
-		$DEBUG && echo "landev: [$default_lan_ifname], wandev=[$default_wan_ifname], vpndev=[$default_vpn_ifname], wwan=[$default_wwan_ifname]"
+		$DEBUG && echo "gateway found: via [$via] dev [$dev] (landev: [$default_lan_ifname], wandev=[$default_wan_ifname, vpndev=[$default_vpn_ifname]])"
 
 		#always add wan or lan to local gateway
-		if [ "$dev" = "$default_lan_ifname" -o "$dev" = "$default_wan_ifname" -o "$dev" = "$default_wwan_ifname" ]; then
+		if [ "$dev" = "$default_lan_ifname" -o "$dev" = "$default_wan_ifname" ]; then
 			#logger -s -t "$LOGGER_TAG" "Set local gateway: dev:$dev, ip:$via"
 			setup_gateway_table $dev $via local_gateway
 			#if lan/wan is tested, then we have no vpn which is working. so clear public gateway
@@ -227,8 +215,7 @@ do
 		#lan/wan local_gateway
 		break;
 	else
-		$DEBUG && echo "gateway NOT found: via [$via] dev [$dev]"
-		$DEBUG && echo "landev: [$default_lan_ifname], wandev=[$default_wan_ifname], vpndev=[$default_vpn_ifname], wwan=[$default_wwan_ifname]"
+		$DEBUG && echo "gateway not found: via $via dev $dev (landev:$default_lan_ifname, wandev=$default_wan_ifname)"
 
 		#logger -s -t "$LOGGER_TAG" "remove local/public gateway: dev:$dev, ip:$via"
 		# remove default route only for interface that was tested! if wan and lan is set
@@ -247,7 +234,7 @@ ip route flush table ping
 ip rule del iif lo fwmark $ip_fwmark priority $ip_rule_priority table ping >/dev/null
 
 # clear iptables used to mark icmp packets
-iptables -w -t mangle -F output_gateway_check
+iptables -t mangle -F output_gateway_check
 
 #in case no single gateway was working but gateway was announced, clear gateways
 if ! $ok; then
